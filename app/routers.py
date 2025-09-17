682439031b933ed6c9ff06107af6864c83b054ba
WorkPC <ct7adh@gmail.com>
2025-09-17 22:27:10 +0100

    Commit Inicial

---
+++ app/routers.py
@@ -0,0 +1,31 @@
+from app import app
+from flask import render_template, url_for
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/index.html")
+def index():
+    return render_template("index.html")
+
+@app.route("/car-list.html")
+def car_list():
+    return render_template("car-list.html")
+
+@app.route("/login.html")
+def login():
+    return render_template("login.html")
+
+@app.route("/registration.html")
+def registration():
+    return render_template("registration.html")
+
+@app.route("/reserva.html")
+def reserva():
+    return render_template("reserva.html")
+
+@app.route("/contact.html")
+def contact():
+    return render_template("contact.html")
+
---
+++ main.py
@@ -0,0 +1,6 @@
+from app import app
+
+
+if __name__ == "__main__":
+    # Colocar o site no ar
+    app.run(debug=True)
